4152ab991cbc917ea08b272ed0e11c2facc819f1
Bram van de Sande <bram@deity.io>
2018-11-01 15:17:22 +0100

    remove the auth.json even if it's not copied

--- bin/purge.sh
+++ bin/purge.sh
@@ -6,6 +6,8 @@ rm -rf src/deity-project/
 rm -rf src/deity-wordpress-api/
 rm -rf src/deity-products-wordpress/
 
+rm -f auth,json
 rm -f docker/magento2/composer/auth.json
 rm -f docker/magento2/.ssh/id_rsa
+
 
