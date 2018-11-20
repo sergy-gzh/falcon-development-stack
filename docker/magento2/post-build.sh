153ee7298314c2cb47d3946a1af7fb78924b4e93
Bram van de Sande <bram@deity.io>
2018-11-20 15:52:18 +0100

    added MAGENTO_DEV to switch between magento vanilla or develop
    added MAGENTO_DEV variable to switch build between
    (MAGENTO_DEV=="Y") our own development branch
    (!MAGENTO_DEV=="Y") vanilla install with deity module

--- docker/magento2/post-build.sh
+++ docker/magento2/post-build.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
 set -e
+
 sudo chown www-data:www-data -R /var/www/html
 
-composer install
-
-sudo find /var/www/html/ -type d -exec chmod 777 {} \; ## check this other-user rights, i'm setting this for the developer on the mounts
-sudo find /var/www/html/ -type f -exec chmod 666 {} \; ## check this other-user rights, i'm setting this for the developer on the mounts
-sudo chmod ugo+x /var/www/html/bin/magento
-    
-/usr/local/bin/install-magento
+if [ "$MAGENTO_DEV" == "Y" ]; then
+    echo 'Magento from deity'
+    composer install
+else
+    if [ ! -f "/var/www/html/composer.json" ]; then
+        echo 'Magento from official magento repo'
+        composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition /var/www/html/
+    else
+        echo 'Magento code exists'
+    fi
+fi
 
 if [ ! -f /var/www/html/var/composer_home/auth.json ]; then
     if [ ! -d /var/www/html/var/composer_home/ ]; then
@@ -18,9 +23,22 @@ if [ ! -f /var/www/html/var/composer_home/auth.json ]; then
     ln -s  /var/www/.composer/auth.json /var/www/html/var/composer_home/auth.json 
 fi
 
+sudo find /var/www/html/ -type d -exec chmod 777 {} \; ## check this other-user rights, i'm setting this for the developer on the mounts
+sudo find /var/www/html/ -type f -exec chmod 666 {} \; ## check this other-user rights, i'm setting this for the developer on the mounts
+sudo chmod ugo+x /var/www/html/bin/magento
+    
+/usr/local/bin/install-magento
+
 bin/magento  admin:user:create  --admin-user="${MAGENTO_NODEUSER_USERNAME}" --admin-password="${MAGENTO_NODEUSER_PASSWORD}" --admin-email="${MAGENTO_NODEUSER_EMAIL}" --admin-firstname="${MAGENTO_NODEUSER_FIRSTNAME}" --admin-lastname="${MAGENTO_NODEUSER_LASTNAME}"
 
 /var/www/html/bin/magento deploy:mode:set developer
+
+if [ "$MAGENTO_DEV" == "Y" ]; then
+    echo "Develpoment mode. not installing modules (should already be in the installation)".
+else
+    composer config repositories.deity-api '{"type": "path", "url": "../packages/api"}'
+    composer require deity/falcon-magento:dev-master
+fi
 
 /var/www/html/bin/magento sampledata:deploy 
 /var/www/html/bin/magento setup:upgrade
