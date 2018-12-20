3cba8dd23b8567d5abce8c9e612ff529a291fa78
Fabian Schmengler <fs@integer-net.de>
2018-12-20 15:34:55 +0100

    Make Magento root dir configurable
    This helps if the Magento project repository has the Magento sources in a subdirectory

--- docker/magento2/post-build.sh
+++ docker/magento2/post-build.sh
@@ -2,36 +2,36 @@
 
 set -e
 
-sudo chown www-data:www-data -R /var/www/html
+sudo chown www-data:www-data -R ${MAGENTO_DIR}
 
 if [ "$MAGENTO_DEV" == "Y" ]; then
     echo 'Magento from deity'
     composer install
 else
-    if [ ! -f "/var/www/html/composer.json" ]; then
+    if [ ! -f "${MAGENTO_DIR}/composer.json" ]; then
         echo 'Magento from official magento repo'
-        composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition="${MAGENTO_VERSION_BRANCH_NAME}" /var/www/html/
+        composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition="${MAGENTO_VERSION_BRANCH_NAME}" ${MAGENTO_DIR}/
     else
         echo 'Magento code exists'
     fi
 fi
 
-if [ ! -f /var/www/html/var/composer_home/auth.json ]; then
-    if [ ! -d /var/www/html/var/composer_home/ ]; then
-        mkdir /var/www/html/var/composer_home/
+if [ ! -f ${MAGENTO_DIR}/var/composer_home/auth.json ]; then
+    if [ ! -d ${MAGENTO_DIR}/var/composer_home/ ]; then
+        mkdir ${MAGENTO_DIR}/var/composer_home/
     fi
-    ln -s  /var/www/.composer/auth.json /var/www/html/var/composer_home/auth.json 
+    ln -s  /var/www/.composer/auth.json ${MAGENTO_DIR}/var/composer_home/auth.json
 fi
 
-sudo find /var/www/html/ -type d -exec chmod 777 {} \; ## check this other-user rights, i'm setting this for the developer on the mounts
-sudo find /var/www/html/ -type f -exec chmod 666 {} \; ## check this other-user rights, i'm setting this for the developer on the mounts
-sudo chmod ugo+x /var/www/html/bin/magento
-    
+sudo find ${MAGENTO_DIR}/ -type d -exec chmod 777 {} \; ## check this other-user rights, i'm setting this for the developer on the mounts
+sudo find ${MAGENTO_DIR}/ -type f -exec chmod 666 {} \; ## check this other-user rights, i'm setting this for the developer on the mounts
+sudo chmod ugo+x ${MAGENTO_DIR}/bin/magento
+
 /usr/local/bin/install-magento
 
-bin/magento  admin:user:create  --admin-user="${MAGENTO_NODEUSER_USERNAME}" --admin-password="${MAGENTO_NODEUSER_PASSWORD}" --admin-email="${MAGENTO_NODEUSER_EMAIL}" --admin-firstname="${MAGENTO_NODEUSER_FIRSTNAME}" --admin-lastname="${MAGENTO_NODEUSER_LASTNAME}"
+${MAGENTO_DIR}/bin/magento  admin:user:create  --admin-user="${MAGENTO_NODEUSER_USERNAME}" --admin-password="${MAGENTO_NODEUSER_PASSWORD}" --admin-email="${MAGENTO_NODEUSER_EMAIL}" --admin-firstname="${MAGENTO_NODEUSER_FIRSTNAME}" --admin-lastname="${MAGENTO_NODEUSER_LASTNAME}"
 
-/var/www/html/bin/magento deploy:mode:set developer
+${MAGENTO_DIR}/bin/magento deploy:mode:set developer
 
 if [ "$MAGENTO_DEV" == "Y" ]; then
     echo "Develpoment mode. not installing modules (should already be in the installation)".
@@ -41,12 +41,12 @@ fi
 
 if [ "$MAGENTO_DEV" != "Y" ]; then
     # Its impossible to run sample data and magento API tests altogether.
-    /var/www/html/bin/magento sampledata:deploy
-    /var/www/html/bin/magento setup:upgrade
+    ${MAGENTO_DIR}/bin/magento sampledata:deploy
+    ${MAGENTO_DIR}/bin/magento setup:upgrade
 fi
 
-sudo chown www-data:www-data -R /var/www/html
+sudo chown www-data:www-data -R ${MAGENTO_DIR}
 
 if [ "$MAGENTO_DEV" != "Y" ]; then
-    bin/magento deploy:mode:set production
+    ${MAGENTO_DIR}/bin/magento deploy:mode:set production
 fi
