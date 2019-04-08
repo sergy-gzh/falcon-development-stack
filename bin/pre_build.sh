c07f5ea45be562419ac5bb27b83a86b75f46a40f
Sergii Gozhedrianov <sergii@deity.io>
2019-04-08 09:41:56 +0200

    removed auth.json requirement, code clean up

--- bin/pre_build.sh
+++ bin/pre_build.sh
@@ -10,18 +10,6 @@
 # init the environment
 .  `dirname "${THIS}"`"/bin/functions.sh"
 
-# Provide composer auth.json
-if [ ! -f auth.json ] && [ ! -f  docker/magento2/composer/auth.json ]; then
-    color_red
-    echo "Copy composer auth.json file next to docker-composer file to proceed (compser auth.json should contain magento credentials)"
-    exit 1
-fi
-
-#move auth.json to the correct docker folder
-if [ ! -f  docker/magento2/composer/auth.json ]; then
-    mv auth.json docker/magento2/composer/
-fi
-
 # Make sure the volumes that need to be mounted exists
 PROJECT_DIRECTORIES=("magento2" "deity-wordpress-api")
 for DIRECTORY in "${PROJECT_DIRECTORIES[@]}"
@@ -34,17 +22,15 @@ git clone git@github.com:deity-io/falcon-wordpress-module.git "${DEITY_STACK_ROO
 
 
 if [ "$MAGENTO_DEV" == "Y" ]; then
-    if [ ! -d "${DEITY_STACK_ROOT}/src/magento2/" ]; then
-        git clone git@github.com:deity-io/falcon-magento2-development.git "${DEITY_STACK_ROOT}/src/magento2/"
-    fi
-else
-    git clone git@github.com:deity-io/falcon-magento2-module.git "${DEITY_STACK_ROOT}/src/deity-magento-api/"
+    color_green
+    echo "Cloning magento development repo"
+    color_reset
+    git clone -b develop git@github.com:deity-io/falcon-magento2-development.git "${DEITY_STACK_ROOT}/src/magento2/"
 fi
 
 # if the project folder doen't exists, run a generator in a deity container, we will map the volumes in the running stack
 if [ ! -d "${DEITY_STACK_ROOT}/src/deity-project" ]; then
     docker build -t falcon "${DEITY_STACK_ROOT}/docker/deity-project/"
-    # rm -rf /home/bram/projects/falcon/src
     docker run -t --rm -v "${DEITY_STACK_ROOT}/src/deity-project":/usr/src falcon:latest /bin/sh -c "cd /usr/src && npx create-falcon-app my-app"
 
     color_green
